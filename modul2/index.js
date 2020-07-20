da48c94ca76ed5d191ecf65a9168d2563acfb7ea
grebenuks <grebenuks2406@gmail.com>
2020-07-20 20:28:43 +0300

    Делает 3 задание

--- modul2/index.js
+++ modul2/index.js
@@ -8,18 +8,19 @@
 // };
 // logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 // logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
 //  */
 //  */ Задание 2
 //  */
 // const calculateEngravingPrice = function (message, pricePerWord) {
-//   const blabla = message.split(' ');
-//   const result = blabla.length * pricePerWord;
+//   const words = message.split(' ');
+//   const result = words.length * pricePerWord;
 //   return result;
 // };
 
-// /
-//  * Проверка работоспособности
-//
+// //
+// //  * Проверка работоспособности
+
 // console.log(
 //   calculateEngravingPrice(
 //     'Proin sociis natoque et magnis parturient montes mus',
@@ -45,3 +46,23 @@
 //  */
 //  */ Задание 3
 //  */
+// const findLongestWord = function (string) {
+//   const str = string.split(' ');
+//   let longest = 0;
+//   let word = null;
+//   for (let i = 0; i < str.length; i++) {
+//     if (longest < str[i].length) {
+//       longest = str[i].length;
+//       word = str[i];
+//     }
+//   }
+//   return word;
+// };
+
+// //  * Вызовы функции для проверки работоспособности твоей реализации.
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
