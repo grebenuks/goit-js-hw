fb30ce365a1c4887904021e332ad353e9e9b6931
grebenuks <grebenuks2406@gmail.com>
2020-07-20 19:05:51 +0300

    Делает второе задание

--- modul2/index.js
+++ modul2/index.js
@@ -1,4 +1,6 @@
-// Задание 1
+//  */
+//  */ Задание 1
+//  */
 // const logItems = function (array) {
 //   for (let i = 0; i < array.length; i++) {
 //     console.log(`'${i + 1} - ${array[i]}'`);
@@ -6,5 +8,40 @@
 // };
 // logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 // logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+//  */
+//  */ Задание 2
+//  */
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//   const blabla = message.split(' ');
+//   const result = blabla.length * pricePerWord;
+//   return result;
+// };
 
-// Задание 2
+// /
+//  * Проверка работоспособности
+//
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     10,
+//   ),
+// ); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   ),
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
+
+//  */
+//  */ Задание 3
+//  */
