c6c9e7e22e9ab22d2e20e664c51e9bdae6875be5
grebenuks <grebenuks2406@gmail.com>
2020-07-22 18:55:16 +0300

    Делает первое задание

---
+++ modul3/index.js
@@ -0,0 +1,32 @@
+//  */
+//  */ Задание 1
+//  */
+// Напиши скрипт, который, для объекта user, последовательно:
+// добавляет поле mood со значением 'happy'
+// заменяет значение hobby на 'skydiving'
+// заменяет значение premium на false
+// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+
+// const user = {
+//   name: 'Mango',
+//   age: 20,
+//   hobby: 'html',
+//   premium: true,
+// };
+
+// user.mood = 'happy';
+// console.log(user);
+
+// user.hobby = 'skydiving';
+// console.log(user);
+
+// user.premium = false;
+// console.log(user);
+
+// const userKeys = Object.keys(user);
+// for (const key of userKeys) {
+//   console.log(`${key} : ${user[key]}`);
+// }
+//  */
+//  */ Задание 2
+//  */
