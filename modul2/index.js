a2637edf1ee9992cca5ae3eb02f042d8a6a84fbb
grebenuks <grebenuks2406@gmail.com>
2020-07-22 16:19:51 +0300

    Исправляет 6 задание

--- modul2/index.js
+++ modul2/index.js
@@ -129,16 +129,39 @@
 // let input = 0;
 // const numbers = [];
 // let total = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   total += Number(numbers[i]);
+// }
+
 // while (input !== null) {
 //   input = prompt('Введите число:', '');
 //   !isNaN(input) ? (total = total + +input) : alert('Введено не число!');
 //   numbers.push(isNaN(input));
-//   for (let i = 0; i < numbers.length; i++) {
-//     total += Number(numbers[i]);
-//   }
 // }
 // console.log(`Общая сумма чисел равна ${total}`);
 
 //  */
 //  */ Задание 7
 //  */
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+// const isLoginValid = function (login) {
+//   // твой код
+// };
+
+// const isLoginUnique = function (allLogins, login) {
+//   // твой код
+// };
+
+// const addLogin = function (allLogins, login) {
+//   // твой код
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+// console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
